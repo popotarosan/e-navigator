e72f95a27903983370108500426fbdbe2979d7e1
Popo Tarou <kisosuu@gmail.com>
2019-03-14 13:06:52 +0000

    [add] 面接登録機能の追加
    面接が登録できるようにコントローラー・ビュー・ルーティングの作成

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,12 +1,31 @@
 class InterviewsController < ApplicationController
-  before_action :set_user, only: [:index]
+  before_action :set_user, only: [:index,:new,:create]
   
   def index
-    @interviews =  Interview.find_by(user_id:@user.id)
+    @interviews =  Interview.where(user_id:@user.id)
     
   end
   
+  def new
+    @interview = Interview.new
+  end
+  
+  def create
+    @interview = Interview.new(interview_params)
+    if @interview.save
+      flash[:success] = "保存に成功しました"
+      redirect_to user_interviews_path(@user)
+    else
+      flash[:failure] = "保存に失敗しました"
+      render 'new'
+    end
+      
+  end
   private
+  
+  def interview_params
+    params.require(:interview).permit(:scheduled_at,:user_id)
+  end
   def set_user
     @user = User.find(params[:user_id])
   end
