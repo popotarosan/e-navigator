a37c637b53a0d6ca2e7ce4af7060a48e4c7cba2e
Popo Tarou <kisosuu@gmail.com>
2019-03-18 06:41:36 +0000

    [fix] UserモデルとInterviewモデルのrelationの修正
    User削除時にInterviewモデルも削除するように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :interviews
+  has_many :interviews,dependent: :destroy
   enum gender: { male: 0, female: 1 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
