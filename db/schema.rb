cc786dbf383ba02b30f571dbff9645d3fd7a3af9
Popo Tarou <kisosuu@gmail.com>
2019-04-12 14:25:58 +0000

    [add]Interviewモデルに面接官のidを保存するためのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190313131219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status"
+    t.integer "interviewer_id"
   end
 
   create_table "users", force: :cascade do |t|
